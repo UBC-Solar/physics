4f0f4cdb58fa4485f71ca4ad6f7ab71ef88759ca
Joshua Riefman <joshuariefman@gmail.com>
2024-08-19 03:02:36 -0700

    Update read-the-docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../../physics'))  # Adjust as necessary
 print(sys.path)
 print(os.listdir(os.getcwd()))
+print(os.listdir(sys.path[0]))
 
 # Configuration file for the Sphinx documentation builder.
 #
