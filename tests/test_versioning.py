fabbc89c23943cc6602a3c56aaa54fc085d6e2a4
Joshua Riefman <joshuariefman@gmail.com>
2024-09-27 00:54:28 -0700

    Fix test

--- tests/test_versioning.py
+++ tests/test_versioning.py
@@ -4,5 +4,5 @@ import importlib
 
 
 def test_version():
-    version_module = importlib.import_module("physics.version")
+    version_module = importlib.import_module("physics")
     assert isinstance(version_module.__version__, str)
