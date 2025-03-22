cc6f660cd5827da4f612502bf1daa4eab985abec
Dekempsy4 <felixtoft09@gmail.com>
2025-03-22 10:39:40 -0700

    adding advanced motor to init.py

--- physics/models/motor/__init__.py
+++ physics/models/motor/__init__.py
@@ -1,7 +1,9 @@
 from .base_motor import BaseMotor
 from .basic_motor import BasicMotor
+from .advanced_motor import AdvancedMotor
 
 __all__ = [
     "BaseMotor",
-    "BasicMotor"
+    "BasicMotor",
+    "AdvancedMotor",
 ]
