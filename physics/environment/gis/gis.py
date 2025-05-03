a5b47d778a836772af511e210407fa60f3d61b44
Joshua Riefman <joshuariefman@gmail.com>
2025-05-02 17:08:04 -0700

    Add documentation of edge case

--- physics/environment/gis/gis.py
+++ physics/environment/gis/gis.py
@@ -102,7 +102,8 @@ class GIS(BaseGIS):
             If there are too few, an error will be returned.
         :param simulation_dt: The simulated tick length.
         :param driving_allowed: A simulation-time boolean where the `True` elements are when we
-            are allowed to drive, and `False` is when we are not.
+            are allowed to drive, and `False` is when we are not. Requires that (at least) the first element is
+            `False` due to the race beginning in the morning before we are allowed to drive.
         :param idle_time: The length of the track in meters.
         :return: A simulation-time array of driving speeds in m/s, or an error if there weren't enough
             laps provided to fill the entire simulation time.
