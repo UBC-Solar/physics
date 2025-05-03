398460babea50548c3c6d4cd810f071643e73844
Joshua Riefman <joshuariefman@gmail.com>
2025-05-02 17:16:17 -0700

    Add calculate_driving_speeds to ABC

--- physics/environment/gis/base_gis.py
+++ physics/environment/gis/base_gis.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from numpy.typing import ArrayLike, NDArray
 
 
 class BaseGIS(ABC):
@@ -25,4 +26,13 @@ class BaseGIS(ABC):
 
     @abstractmethod
     def calculate_current_heading_array(self) -> np.ndarray:
+        raise NotImplementedError
+
+    def calculate_driving_speeds(
+            self,
+            average_lap_speeds: ArrayLike,
+            simulation_dt: int,
+            driving_allowed: ArrayLike,
+            idle_time: int
+    ) -> NDArray[float]:
         raise NotImplementedError
