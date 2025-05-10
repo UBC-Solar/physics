e1e5d78557420cc22138d60d07586ad78d45fa0c
Joshua Riefman <joshuariefman@gmail.com>
2025-05-10 00:40:38 -0700

    Refactor Kalman filter entirely

--- physics/models/battery/kalman_filter.py
+++ physics/models/battery/kalman_filter.py
@@ -1,159 +1,185 @@
 import numpy as np
-from scipy import optimize
-from filterpy.kalman import ExtendedKalmanFilter as EKF
-from physics.models.battery.battery_config import BatteryModelConfig
+from filterpy.kalman import ExtendedKalmanFilter
+from typing import Protocol, runtime_checkable, cast, Callable
+from physics.models.battery import EquivalentCircuitModelConfig
+from numpy.typing import NDArray
 
 
-class EKF_SOC:
-    def __init__(self, battery_config: BatteryModelConfig, initial_SOC=1, initial_Uc=0):
+@runtime_checkable
+class FilteredBatteryModelConfig(Protocol):
+    """
+    A specification for a configuration object which contains the requisite data to specify
+    a `FilteredBatteryModel`.
+    """
+    @property
+    def battery_model_config(self) -> EquivalentCircuitModelConfig:
         """
-        EKF_SOC represents the Kalman filter used for predicting state of charge.
+        Configuration of the underlying `EquivalentCircuitModel`.
+        """
+        ...
 
-        :param BatteryModelConfig battery_config: Contains the HPPC parameters of the battery model.
-        :param float initial_SOC: Initial state of charge of the battery (ranges from 0 to 1 inclusive, default is 1).
-        :param float initial_Uc: Initial polarization voltage of the battery in volts (default is 0).
+    @property
+    def process_noise_matrix(self) -> NDArray[float]:
+        """
+        A 2x2 matrix containing the process noise covariance matrix where [0, 0] is the SOC evolution
+        noise and [1, 1] is the polarization potential evolution noise.
+        """
+        ...
+
+    @property
+    def state_covariance_matrix(self) -> NDArray[float]:
+        """
+        A 2x2 matrix containing the state covariance matrix where [0, 0] is the SOC covariance
+        noise and [1, 1] is the polarization potential covariance.
+        """
+        ...
+
+    @property
+    def measurement_noise_vector(self) -> NDArray[float]:
+        """
+        A 1x1 vector containing the noise expected in the terminal voltage measurement.
+        """
+        ...
+
+
+class FilteredBatteryModel:
+    """
+    `FilteredBatteryModel` is a first-order Thevenin equivalent model of a lithium-ion battery packed, wrapped
+    in a Kalman filter which uses voltage measurements with model predictions.
+    """
+    def __init__(
+            self,
+            battery_config: FilteredBatteryModelConfig,
+            initial_SOC: float = 1.0,
+            initial_Uc: float = 0.0,
+            alpha: float = 0.9
+    ):
+        """
+        :param FilteredBatteryModelConfig battery_config: Contains Kalman filter state estimation configuration and
+            underlying equivalent circuit model configuration.
+        :param float initial_SOC: Initial SOC of the battery, in the range (0, 1].
+        :param float initial_Uc: Initial polarization voltage of the battery in Volts.
         """
         # Initial state
-        self.SOC = initial_SOC
-        self.Uc = initial_Uc  # Polarization Voltage
+        self._SOC = initial_SOC     # State of Charge
+        self._Uc = initial_Uc       # Polarization Voltage
 
         # Load Config data
-        self.Q_total = battery_config.Q_total
-        SOC_data = battery_config.SOC_data
-        Uoc_data = battery_config.Uoc_data
-        R_0_data = battery_config.R_0_data
-        R_P_data = battery_config.R_P_data
-        C_P_data = battery_config.C_P_data
+        self._Q_total = battery_config.battery_model_config.Q_total
 
-        def quintic_polynomial(x, x0, x1, x2, x3, x4, x5, x6, x7):
-            return np.polyval([x0, x1, x2, x3, x4, x5, x6, x7], x)
+        # These `cast` calls just promise to the type-checker that these will map floats to floats
+        self._U_oc = cast(Callable[[float], float], battery_config.battery_model_config.get_Uoc)
+        self._R_0 = cast(Callable[[float], float], battery_config.battery_model_config.get_R_0)
+        self._R_P = cast(Callable[[float], float], battery_config.battery_model_config.get_R_P)
+        self._C_P = cast(Callable[[float], float], battery_config.battery_model_config.get_C_P)
 
-        U_oc_coefficients, _ = optimize.curve_fit(quintic_polynomial, SOC_data, Uoc_data)
-        R_0_coefficients, _ = optimize.curve_fit(quintic_polynomial, SOC_data, R_0_data)
-        R_P_coefficients, _ = optimize.curve_fit(quintic_polynomial, SOC_data, R_P_data)
-        C_P_coefficients, _ = optimize.curve_fit(quintic_polynomial, SOC_data, C_P_data)
-        self.U_oc = lambda soc: np.polyval(U_oc_coefficients, soc)  # Open-circuit voltage as a function of SOC
-        self.R_0 = lambda soc: np.polyval(R_0_coefficients, soc)  # Resistance as a function of SOC
-        self.R_P = lambda soc: np.polyval(R_P_coefficients, soc)  # Resistance as a function of SOC
-        self.C_P = lambda soc: np.polyval(C_P_coefficients, soc)  # Resistance as a function of SOC
-        self.Uoc_derivative = lambda soc: np.polyval(np.polyder(U_oc_coefficients),
-                                                     np.minimum(1.0, soc))  # Derivative of Uoc wrt SOC
-        self.R_0_derivative = lambda soc: np.polyval(np.polyder(R_0_coefficients), np.minimum(1.0, soc))
+        self._tau: Callable[[float], float] = lambda soc: self._R_P(soc) * self._C_P(soc)  # Characteristic Time in s
 
-        self.tau = lambda soc: self.R_P(soc) * self.C_P(soc)
+        def central_difference_derivative(func, value, h=1e-6):
+            """
+            Compute the derivative of an arbitrary function `func` at `SOC` using central difference.
 
-        # initializing the ekf object
-        self.ekf = EKF(dim_x=2, dim_z=1)
-        self.ekf.x = np.array([self.SOC, self.Uc])
-        self.ekf.Q = np.diag([
-            1e-10 * 0.1,  # add floor to maintain numerical stability
-            1e-6 * 0.1
-        ])
-        self.ekf.P = np.diag(
-            [1e-2 * 0.5,
-             1e-1]
+            :param func: The function to differentiate.
+            :param value: The point at which to compute the derivative.
+            :param h: Step size for the finite difference.
+            :return: The derivative of the function at `SOC`.
+            """
+            return (func(value + h) - func(value - h)) / (2 * h)
+
+        self._dUoc_dSOC = lambda soc: central_difference_derivative(self._U_oc, np.minimum(1.0, soc))  # dUOC wrt to SOC
+        self._dR0_dSOC = lambda soc: central_difference_derivative(self._R_0, np.minimum(1.0, soc))    # dR0 wrt to SOC
+
+        # Initializing the EKF object
+        self._ekf = ExtendedKalmanFilter(dim_x=2, dim_z=1)
+
+        # State Vector
+        self._ekf.x = np.array([
+            self._SOC,
+            self._Uc]
         )
-        self.ekf.R = np.eye(1) * 1e0 * 0.5
+
+        self._ekf.P = battery_config.state_covariance_matrix
+        self._ekf.Q = battery_config.process_noise_matrix
+        self._ekf.R = battery_config.measurement_noise_vector
 
         # For logs
+        self._alpha = alpha
         self._filtered_I = 0
-        self.predicted_measurement = 0
+        self._predicted_measurement = 0
 
-    def get_SOC(self):
+    @property
+    def SOC(self) -> float:
         """
-        Return the current state of charge of the battery.
+        Return the current SOC of the battery.
 
         :return: The current state of charge.
-        :rtype: float
         """
-        return self.SOC
+        return self._SOC
 
-    def get_Uc(self):
+    @property
+    def Uc(self) -> float:
         """
         Return the polarization voltage of the battery.
 
         :return: The current polarization voltage.
-        :rtype: float
         """
-        return self.Uc
+        return self._Uc
 
-    def get_predicted_Ut(self):
+    @property
+    def Ut(self) -> float:
         """
         Return the predicted terminal voltage for the last prediction step.
 
         :return: The predicted terminal voltage.
-        :rtype: float
         """
-        return self.predicted_measurement
+        return self._predicted_measurement
 
-    def update_filter(self, measured_Ut, I):
+    def update_filter(self, measured_Ut, current):
         """
         Update the filter based on a new measurement and the predicted state.
         This function should be called after `predict_state` in a typical predict-update workflow.
 
         :param float measured_Ut: The actual voltage across the terminals of the battery.
-        :param float I: The current being sourced by the battery.
+        :param float current: The current being sourced by the battery.
         """
-        alpha = 0.9
-        self._filtered_I = alpha * self._filtered_I + (1 - alpha) * I
+        # Simple low-pass filter to current
+        self._filtered_I = self._alpha * self._filtered_I + (1 - self._alpha) * current
 
-        self.ekf.update(z=measured_Ut, HJacobian=self._measurement_jacobian, Hx=self._measurement_function)
+        self._ekf.update(z=measured_Ut, HJacobian=self._measurement_jacobian, Hx=self._measurement_function)
 
-        self.SOC, self.Uc = self.ekf.x
-        self.SOC = np.clip(self.SOC, 0.0, 1.1)
-        # print(f"Kalman gain: {self.ekf.K}")
-        # Uoc = self.U_oc(self.SOC)
-        # R0 = self.R_0(self.SOC)
-        # self.predicted_measurement = Uoc - self.Uc - R0 * I
+        self._SOC, self._Uc = self._ekf.x
+        self._SOC = np.clip(self._SOC, 0.0, 1.1)
 
-    def predict_state(self, I, time_step):
+    def predict_state(self, current, time_step):
         """
         Predict the next evolution of the state vector (SOC, Uc).
         This function should be called before updating the filter in a typical predict-update workflow.
 
-        :param float I: The current being sourced by the battery. Positive indicates current being drawn.
+        :param float current: The current being sourced by the battery.
+            Sign convention is that positive indicates current being drawn.
         :param float time_step: Time elapsed between this prediction and the last updated state of the filter (seconds).
         """
-        # check_current(I)
         # Control matrix B (for input current I_k)
-        self.ekf.B = np.array(
-            [-time_step / self.Q_total, self.R_P(self.SOC) * (1 - np.exp(-time_step / self.tau(self.SOC)))])
-        self.ekf.F = self._state_jacobian(time_step)
+        self._ekf.B = np.array([
+            -time_step / self._Q_total,
+            self._R_P(self._SOC) * (1 - np.exp(-time_step / (self._tau(self._SOC)))),
+        ])
+        self._ekf.F = self._state_jacobian(time_step)
 
-        #     # 🔧 Incorporate uncertainty in current measurement into Q
-        # I_noise_std = 0.5  # adjust based on sensor datasheet or empirical tests
-        # dq = (time_step / self.Q_total) * I_noise_std
-        # duc = self.R_P(self.SOC) * (1 - np.exp(-time_step / self.tau(self.SOC))) * I_noise_std
-        # # if I > 5:
-        # #     print(f"dq: {dq * 1e2}, duc: {duc * 1e4}, I: {I}")
-        #
-        # self.ekf.Q = np.diag([
-        #     1e-1 * 0.1,  # add floor to maintain numerical stability
-        #     1e3
-        # ])
+        self._ekf.predict(u=current)
+        self._SOC, self._Uc = self._ekf.x
 
-        self.ekf.predict(u=I)
-        self.SOC, self.Uc = self.ekf.x
-
-        # print(f'ekf prediction: {self.ekf.x_prior}')
-
-    def predict_then_update(self, measured_Ut, I, time_step):
+    def predict_then_update(self, measured_Ut, current, time_step):
         """
         Predict the next evolution of the state vector (SOC, Uc), then update the filter
         based on this prediction and a measurement. Abstracts the full predict-update workflow of the EKF.
 
         :param float measured_Ut: The actual voltage across the terminals of the battery.
-        :param float I: The current being sourced by the battery. Positive indicates current being drawn.
+        :param float current: The current being sourced by the battery. Positive indicates current being drawn.
         :param float time_step: Time elapsed between this prediction and the last updated state of the filter (seconds).
         """
-        # check_current(I)
-        # check_Terminal_V(measured_Ut)
-
-        self.predict_state(I, time_step)
-        # print(f'predicted: {self.ekf.x_prior}')
-
-        self.update_filter(measured_Ut, I)
-        # print(f'SOC: {self.ekf.x[0]}, Uc: {self.ekf.x[1]}')
+        self.predict_state(current, time_step)
+        self.update_filter(measured_Ut, current)
 
     def _state_jacobian(self, time_step):
         """
@@ -163,32 +189,32 @@ class EKF_SOC:
         :return: The state Jacobian matrix.
         :rtype: np.ndarray
         """
-        return np.array([[1, 0], [0, np.exp(-time_step / self.tau(self.SOC))]])
+        return np.array([[1, 0], [0, np.exp(-time_step / self._tau(self._SOC))]])
 
     def _measurement_jacobian(self, x):
         """
         Return the measurement Jacobian matrix for the current state vector.
 
-        :param list[float, float] x: The state vector [SOC, Uc].
+        :param list[float, float] x: The state vector [_SOC, _Uc].
         :return: The measurement Jacobian matrix.
         :rtype: np.ndarray
         """
         SOC = x[0]
-        dUoc_dSOC = self.Uoc_derivative(SOC)
-        dR0_dSOC = self.R_0_derivative(SOC)
+        dUoc_dSOC = self._dUoc_dSOC(SOC)
+        dR0_dSOC = self._dR0_dSOC(SOC)
+
         return np.array([[dUoc_dSOC - dR0_dSOC * self._filtered_I, -1]])
 
-    def _measurement_function(self, x):
+    def _measurement_function(self, x) -> float:
         """
         Return the measurement function relating terminal voltage to SOC and polarization voltage.
 
-        :param list[float, float] x: The state vector [SOC, Uc].
-        :param float I: The current being sourced by the battery.
+        :param list[float, float] x: The state vector [_SOC, _Uc].
         :return: The predicted terminal voltage.
-        :rtype: float
         """
         SOC, Uc = x
-        Uoc = self.U_oc(SOC)
-        R0 = self.R_0(SOC)
-        self.predicted_measurement = Uoc - Uc - R0 * self._filtered_I
-        return self.predicted_measurement
+        Uoc = self._U_oc(SOC)
+        R0 = self._R_0(SOC)
+        self._predicted_measurement = Uoc - Uc - R0 * self._filtered_I
+
+        return self._predicted_measurement
