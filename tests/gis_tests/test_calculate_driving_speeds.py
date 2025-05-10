98da9e7868f403962b9819911dfbfa65d0ce7d2a
Joshua Riefman <joshuariefman@gmail.com>
2025-05-10 12:08:35 -0700

    Fix lib.rs (2)

--- tests/gis_tests/test_calculate_driving_speeds.py
+++ tests/gis_tests/test_calculate_driving_speeds.py
@@ -1,4 +1,4 @@
-import core
+import physics_rs
 import numpy as np
 import pytest
 
@@ -13,7 +13,7 @@ def test_calculate_driving_speeds_simple():
 
     idle_time = 3
     driving_allowed = np.array([0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype=bool)
-    driving_speeds = core.get_driving_speeds(average_speeds, simulation_dt, driving_allowed, track_length, idle_time)
+    driving_speeds = physics_rs.get_driving_speeds(average_speeds, simulation_dt, driving_allowed, track_length, idle_time)
 
     assert np.all(np.equal(driving_speeds, [0, 10, 10, 10, 14, 14, 0, 0, 0, 0, 0, 30, 0, 0, 0, 10, 10, 10, 20, 20, 30]))
 
@@ -29,7 +29,7 @@ def test_calculate_driving_speeds_fp():
 
     idle_time = 3
     driving_allowed = np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype=bool)
-    driving_speeds = core.get_driving_speeds(average_speeds, simulation_dt, driving_allowed, track_length, idle_time)
+    driving_speeds = physics_rs.get_driving_speeds(average_speeds, simulation_dt, driving_allowed, track_length, idle_time)
 
     assert np.allclose(driving_speeds, np.array([0, 0, 9.9, 9.9, 9.9, 9.9, 30.7, 15.2, 15.2, 15.2, 15.3, 15.3]), atol=1e-3)
 
@@ -44,7 +44,7 @@ def test_calculate_driving_speeds_fp_with_tick():
 
     idle_time = 3
     driving_allowed = np.array([0, 0, 1, 1, 1, 1, 1, 1], dtype=bool)
-    driving_speeds = core.get_driving_speeds(average_speeds, simulation_dt, driving_allowed, track_length, idle_time)
+    driving_speeds = physics_rs.get_driving_speeds(average_speeds, simulation_dt, driving_allowed, track_length, idle_time)
 
     assert np.allclose(driving_speeds, np.array([0, 0, 10.2, 10.2, 30.7, 15.2, 15.2, 15.3]), atol=1e-3)
     pass
@@ -65,6 +65,6 @@ def test_failure():
     with pytest.raises(ValueError):
         core.get_driving_speeds(average_speeds_too_few, simulation_dt, driving_allowed, track_length, idle_time)
 
-    driving_speeds = core.get_driving_speeds(average_speeds_too_many, simulation_dt, driving_allowed, track_length, idle_time)
+    driving_speeds = physics_rs.get_driving_speeds(average_speeds_too_many, simulation_dt, driving_allowed, track_length, idle_time)
 
     assert np.all(np.equal(driving_speeds, [0, 10, 10, 10, 14, 14, 0, 0, 0, 0, 0, 30, 0, 0, 0, 10, 10, 10, 20, 20, 30]))
