58fbf7139035298f9d95e4b6308ca2482d32a867
Joshua Riefman <joshuariefman@gmail.com>
2025-05-10 10:45:40 -0700

    Fix documentation

--- physics/environment/gis/gis.py
+++ physics/environment/gis/gis.py
@@ -104,7 +104,7 @@ class GIS(BaseGIS):
         :param driving_allowed: A simulation-time boolean where the `True` elements are when we
             are allowed to drive, and `False` is when we are not. Requires that (at least) the first element is
             `False` due to the race beginning in the morning before we are allowed to drive.
-        :param idle_time: The length of the track in meters.
+        :param idle_time: The length of time to pause driving upon processing a "0m/s" average speed.
         :return: A simulation-time array of driving speeds in m/s, or an error if there weren't enough
             laps provided to fill the entire simulation time.
         """
