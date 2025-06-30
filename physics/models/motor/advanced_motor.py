9b8b37ce316f11fb530630766e2fbc6ea572bab2
Joshua Riefman <joshuariefman@gmail.com>
2025-06-29 19:35:30 -0700

    Revert "Add coefficients for tuning"
    This reverts commit b5a434df0bf452fd6abcab69ea178167b330ba9d.

--- physics/models/motor/advanced_motor.py
+++ physics/models/motor/advanced_motor.py
@@ -5,9 +5,9 @@ from physics.models.motor import BasicMotor
 
 
 class AdvancedMotor(BasicMotor):
-    def __init__(self, cornering_coefficient=15, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cornering_coefficient = cornering_coefficient
+        self.cornering_coefficient = 15  # tuned to Day 1 and 3 FSGP data
 
     def calculate_energy_in(self, required_speed_kmh, gradients, wind_speeds, tick, coords):
         """
